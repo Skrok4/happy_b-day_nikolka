4af4688d23a8d0bfb62bac443c79476b83a932c3
Shevchenko Oleksandr <73699809+Skrok4@users.noreply.github.com>
2023-02-22 21:32:05 +0200

    Create slidesPluguin

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const slides = document.querySelectorAll('.slide')
-for (const slide of slides) {
-    slide.addEventListener('click', () =>{
-    clearActiveClasses()
-        slide.classList.add('active')
-    })
+function slidesPluguin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+    slides[activeSlide].classList('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () =>{
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+       })
+     }
 }
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-    slide.classList.remove('active')
-    })
-}
+
+slidesPluguin()
